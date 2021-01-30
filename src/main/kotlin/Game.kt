614bfc1f4c55d1fd8fc743a0a5e8f738c8ad1682
Carter Springall <carter.springall01@gmail.com>
2021-01-30 18:23:26 +0000

    Commenting code

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,37 +1,38 @@
 import kotlin.system.exitProcess
 
-class Game(val dealer: Dealer, val player: Player){
+class Game(val dealer: Dealer, val player: Player) {
     var gameOver = false
-    init{
-        while (!gameOver){
+
+    init {
+        while (!gameOver) {
             println("Type \"quit\" to leave.")
             println("-------------------------------------------------------------------")
-            for(i in 1..2){
+            for (i in 1..2) {
                 dealer.dealCard(player)
                 dealer.drawCard()
             }
             dealer.revealCard(0)
             var roundOver = false
-            while(!roundOver) {
+            while (!roundOver) {
                 var move = player.playerMove()
-                if(move == "quit") {
+                if (move == "quit") {
                     exitProcess(1)
-                }else if(move == "twist"){
+                } else if (move == "twist") {
                     dealer.dealCard(player)
-                    if(isBust(player.hand)){
+                    if (isBust(player.hand)) {
                         roundOver = true
                     }
-                }else{
-                    while(getSum(player.hand) > getSum(dealer.hand)){
+                } else {
+                    while (getSum(player.hand) > getSum(dealer.hand)) {
                         dealer.drawCard()
                     }
                     roundOver = true
                 }
             }
-            if(isBust(player.hand) || ((getSum(player.hand) <= getSum(dealer.hand)) && !isBust(dealer.hand))){
+            if (isBust(player.hand) || ((getSum(player.hand) <= getSum(dealer.hand)) && !isBust(dealer.hand))) {
                 println("You lost.")
                 dealer.printHand()
-            }else{
+            } else {
                 println("You win, dealer went bust.")
                 dealer.printHand()
             }
@@ -40,16 +41,16 @@ class Game(val dealer: Dealer, val player: Player){
     }
 
     //check if a player is bust
-    fun isBust(hand: MutableList<Card>): Boolean{
+    fun isBust(hand: MutableList<Card>): Boolean {
         return getSum(hand) > 21
     }
 
     //calculates total sum of cards in a hand
-    fun getSum(hand : MutableList<Card>): Int{
+    fun getSum(hand: MutableList<Card>): Int {
         var sum = 0
         var hasAce = false
         hand.forEach {
-            when(it.value){
+            when (it.value) {
                 "2" -> sum += 2
                 "3" -> sum += 3
                 "4" -> sum += 4
@@ -65,10 +66,10 @@ class Game(val dealer: Dealer, val player: Player){
                 "A" -> hasAce = true
             }
         }
-        if(hasAce){
-            if(sum<=10){
+        if (hasAce) {
+            if (sum <= 10) {
                 sum += 11
-            }else{
+            } else {
                 sum += 1
             }
         }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,31 +1,30 @@
-import org.jetbrains.annotations.Nullable
-
-class Player(val name: String){
+class Player(val name: String) {
     var hand = mutableListOf<Card>()
 
     //players move
-    fun playerMove(): String{
+    fun playerMove(): String {
         printHand()
         return twistOrStick()
     }
 
     //player chooses to keep current cards or draw one more
-    private fun twistOrStick(): String{
+    private fun twistOrStick(): String {
         var validAnswer = false
         var answer = ""
-        while(!validAnswer) {
+        while (!validAnswer) {
             println("Would you like to twist or stick?")
             answer = readLine().toString()
-            if(answer == "twist" || answer == "stick" || answer == "quit"){
+            if (answer == "twist" || answer == "stick" || answer == "quit") {
                 validAnswer = true
-            }else{
+            } else {
                 println("Invalid answer, please enter \"twist\" or \"stick\".")
             }
         }
         return answer
     }
+
     //prints players hand
-    private fun printHand(){
+    private fun printHand() {
         println("Your cards are:")
         hand.forEach {
             println(it.value + " of " + it.suit)
